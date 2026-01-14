2af0516fa559c1939b62518797f106b6a601d45e
Muhammad Yaseen <Muhammad.Yaseen@intersyslimited.local>
2026-01-14 17:59:06 +0500

    working

---
+++ src/app/device/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Device() {
+    return (
+        <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+                <h2>Device</h2>
+            </main>
+        </div>
+    );
+}
